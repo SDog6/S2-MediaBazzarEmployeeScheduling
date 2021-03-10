cbfef12e9a6fb07be8437b26d62c60ba761f1705
Preslavski,Kaloyan K.S <k.preslavski@student.fontys.nl>
2021-03-10 18:11:41 +0200

    Management

---
+++ Management_PRJ/MediaBazaar/MediaBazaar/Department.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaar
+{
+    public class Department
+    {
+        private string name;
+        private string type;
+
+        List<Employee> Employees;
+
+        public string Name
+        {
+            get { return this.name; }
+            set
+            {
+                if (value == "")
+                { this.name = "Unknown"; }
+                else
+                { this.name = value; }
+            }
+        }
+
+
+        public Department(string name)
+        {
+            this.Name = name;
+            this.Employees = new List<Employee>();
+        }
+
+        public Employee GetEmployee(string BSN)
+        {
+            foreach(Employee e in this.Employees)
+            {
+                if(e.BSNp == BSN)
+                {
+                    return e;
+                }
+            }
+            return null;
+        }
+        public Employee[] GetEmployees()
+        {
+            return this.Employees.ToArray();
+        }
+    }
+}
