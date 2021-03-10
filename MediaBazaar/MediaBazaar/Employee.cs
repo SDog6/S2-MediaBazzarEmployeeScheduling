5c5fc1d5daba72f0e0f0dc8681d9e4e38c3a7148
Hristov, Svetoslav S.B <s.hristov@student.fontys.nl>
2021-03-10 14:52:47 +0200

    Account, Employee, Person, Contract, Address classes pushed

---
+++ MediaBazaar/MediaBazaar/Employee.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazaar
+{
+    public class Employee
+    {
+        private Person employeeInfo;
+        private Person contactPerson;
+
+        private DateTime dateOfBirth;
+        private string BSN;
+
+        private Contract contract;
+
+        public Employee(Person employeeInfo, Person contactPerson, DateTime dateOfBirth, string BSN, Contract contract)
+        {
+            this.employeeInfo = employeeInfo;
+            this.contactPerson = contactPerson;
+            this.dateOfBirth = dateOfBirth;
+            this.BSN = BSN;
+            this.contract = contract;
+        }
+
+        public string getEmployeeInfo()
+        {
+            return $"{employeeInfo}";
+        }
+
+        public string getContactInfo()
+        {
+            return $"{contactPerson}";
+        }
+
+        public void fireEmployee(DateTime dateFired, string reason)
+        {
+            contract.Terminate(dateFired, reason);
+        }
+
+        public bool isFired()
+        {
+            return contract.isTerminated();
+        }
+    }
+}
