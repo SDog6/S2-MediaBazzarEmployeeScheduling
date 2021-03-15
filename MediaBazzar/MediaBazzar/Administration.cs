88cfd027657cdcf9c45d063f8937fd120186bf93
Hristov, Svetoslav S.B <s.hristov@student.fontys.nl>
2021-03-15 19:37:25 +0200

    upload project from master

---
+++ MediaBazzar/MediaBazzar/Administration.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazzar
+{
+    class Administration
+    {
+
+      public static  List<Account> accounts = new List<Account>();
+         public static List<Employee> employees = new List<Employee>();
+
+
+        public void AddAccount(Account a)
+        {
+            accounts.Add(a);
+        }
+        public void AddEmployee(Employee e)
+        {
+            employees.Add(e);
+        }
+
+        public Employee GetEmployee(string email)
+        {
+            foreach (Employee item in employees)
+            {
+                if (item.getEmail() == email)
+                {
+                    return item;
+                }
+            }
+            return null;
+        }
+
+        public Account GetAccount(string username)
+        {
+            foreach (Account item in accounts)
+            {
+                if (username == item.getUsername())
+                {
+                    return item;
+                }
+
+            }
+            return null;
+        }
+        public static List<Employee> GetEmployees()
+        {
+            return employees;
+        }
+
+        public List<Account> GetAccounts()
+        {
+            return accounts;
+        }
+
+    }
+}
---
+++ MediaBazzar/MediaBazzar/Employee.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MediaBazzar
+{
+    class Employee : Person
+    {
+
+        private Person contactPerson;
+        private string dateOfBirth;
+        private string BSN;
+        private Contract contract;
+        private Account account;
+
+        public string BSNp { get { return this.BSN; } }
+
+
+        public Employee(string firstName, string lastName, string phoneNumber, Address address, string email, Person contactPerson, string dateOfBirth, string BSN, Contract contract) : base(firstName, lastName, phoneNumber, address, email)
+        {
+            this.contactPerson = contactPerson;
+            this.dateOfBirth = dateOfBirth;
+            this.BSN = BSN;
+            this.contract = contract;
+            this.account = null;
+        }
+
+        public string getContactInfo()
+        {
+            return $"{contactPerson}";
+        }
+
+        public void SetAccount(Account a)
+        {
+            this.account = a;
+        }
+
+        public void fireEmployee(DateTime dateFired, string reason)
+        {
+            contract.Terminate(dateFired, reason);
+        }
+
+        public bool isFired()
+        {
+            return contract.isTerminated();
+        }
+        public string GetInfo()
+        {
+            string inf = base.ToString();
+            return inf;
+        }
+
+    }
+}
