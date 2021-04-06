909611274d72feef77cc04200e99a37a6655ffb4
Hristov,Hristo H <h.hristov-ab@student.fontys.nl>
2021-04-06 10:53:23 +0200

    Created basic html log in / register part of website

---
+++ MediaBazzarWebsite/login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+
+
+<section>
+    <h3>Log into your account</h3>
+   <form action="/action_page.php">
+  <label for="fname">Username:</label><br>
+  <input type="text" id="username" name="fname" value=""><br>
+  <label for="lname">Password:</label><br>
+  <input type="text" id="password" name="password" value=""><br><br>
+  <input type="submit" value="Login">
+</form> 
+</section>
+
+
+</html>
---
+++ MediaBazzarWebsite/register.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<section>
+    <h3>Register</h3>
+   <form action="register.php">
+  <label for="username">Username:</label><br>
+  <input type="text" id="username" name="username" value=""><br>
+  <label for="password">Password:</label><br>
+  <input type="text" id="password" name="password" value=""><br>
+   <label for="rpassword">Repeat Password:</label><br>
+  <input type="text" id="rpassword" name="rpassword" value=""><br>
+    <label for="Email">Email:</label><br>
+  <input type="text" id="Email" name="Email" value=""><br><br>
+  <input type="submit" value="Login">
+</form> 
+</section>
+
+</html>
