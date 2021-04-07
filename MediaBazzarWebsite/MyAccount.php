df72dd2491edb9d5152cdb2ee5c1b7b8e16e5dda
Lodowica,Brandon B.C <b.lodowica@student.fontys.nl>
2021-04-07 12:21:54 +0200

    Css Login from and Nav-bar

---
+++ MediaBazzarWebsite/MyAccount.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Account</title>
+    <link rel="stylesheet" type="text/css" href="LoginStyle.css">
+
+<ul class="topnav">
+  <li><a class="active" href="Home.php">Home</a></li>
+  <li><a href="MyAccount.php">My Account</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="login.php">Login</a></li>
+</head>
+<body>
+    
+</body>
+</html>
