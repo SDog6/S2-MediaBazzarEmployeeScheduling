16cc7822f0dfabaa39c5969cbfaf1a227b9f536e
Lodowica,Brandon B.C <b.lodowica@student.fontys.nl>
2021-04-07 12:20:50 +0200

    Css Login from and Nav-bar

--- MediaBazzarWebsite/login.php
+++ MediaBazzarWebsite/login.php
@@ -1,16 +1,38 @@
 <!DOCTYPE html>
+<link rel="stylesheet" type="text/css" href="LoginStyle.css">
+
+<ul class="topnav">
+  <li><a class="active" href="index.php">Home</a></li>
+  <li><a href="MyAccount.php">My Account</a></li>
+  <li><a href="#contact">Contact</a></li>
+  <li><a href="login.php">Login</a></li>
+
+</ul>
 
 
-<section>
-    <h3>Log into your account</h3>
-   <form action="/action_page.php">
-  <label for="fname">Username:</label><br>
-  <input type="text" id="username" name="fname" value=""><br>
-  <label for="lname">Password:</label><br>
-  <input type="text" id="password" name="password" value=""><br><br>
-  <input type="submit" value="Login">
-</form> 
-</section>
+<div class="header">
+  	<h2>Login</h2>
+  </div>
+	 
+  <form method="post" action="login.php">
+  	
+  	<div class="input-group">
+  		<label>Username</label>
+  		<input type="text" name="username" >
+  	</div>
+  	<div class="input-group">
+  		<label>Password</label>
+  		<input type="password" name="password">
+  	</div>
+  	<div class="input-group">
+  		<button type="submit" class="btn" name="login_user">Login</button>
+  	</div>
+  	<p>
+  		Forgot Password? <a href="registration.php">Reset password</a>
+  	</p>
+  </form>
 
-
+ 
+ 
+</body>
 </html>
