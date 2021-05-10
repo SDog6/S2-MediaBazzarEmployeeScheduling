c4cf7d3ac8a2f319e387024c848d75131252411f
Lodowica,Brandon B.C <b.lodowica@student.fontys.nl>
2021-05-10 17:20:51 +0200

    General information is now shown in the myaccount page

--- MediaBazzarWebsite/MyAccount.php
+++ MediaBazzarWebsite/MyAccount.php
@@ -21,11 +21,53 @@ echo "<a href='changeemail.php' >Change email</a>";
 echo "<br>";
 echo "<a href='changepassword.php' >Change password</a>";
 
-$currenAccountId = $_SESSION['accountId'];
-$sql = "Select name From account inner join employee where accountId=:accountId" ;
-$stmt->bindParam(:accountId, $currenAccountId);
-$stmt = $this->Connect()->prepare($sql);
-$stmt->execute();
+$host ="studmysql01.fhict.local";
+$user = 'dbi457108';
+$dbpass = 'NewPassword123';
+$dbName = 'dbi457108';
+$dsn = 'mysql:host='.$host.';dbname='.$dbName;
+
+try
+{
+$pdo = new PDO($dsn,$user,$dbpass);
+$sql = "SELECT person.firstName, person.lastName, person.phoneNumber,person.email, person.addressId, account.username, employee.dateOfBirth, employee.role
+ FROM account LEFT JOIN employee ON account.id = employee.accountId LEFT JOIN person ON employee.personId = person.id 
+ WHERE account.id LIKE :accountId";
+$stm = $pdo->prepare($sql);
+$stm->bindParam(":accountId", $id);
+$stm->execute();
+
+  
+$result = $stm->setFetchMode(PDO::FETCH_ASSOC);
+
+foreach($stm->fetchAll() as $k=>$v) {
+    echo "<br><br><br><br>";
+    echo "firstname: " . $v['firstName']; 
+    echo "<br>";
+    echo "lastname: " . $v['lastName'];
+    echo "<br>";
+    echo "PhoneNumber:" . $v['phoneNumber'];
+    echo "<br>";
+    echo "Email adress: " . $v['email'];
+    echo "<br>";
+    echo "Adress: " . $v['addressId'];
+    echo "<br>";
+    echo "Username: " . $v['username'];
+    echo "<br>";
+    echo "Date of Birth: " . $v['dateOfBirth'];
+    echo "<br>";
+    echo "Role : " . $v['role'];
+
+}
+     $pdo = null;
+    }
+     catch(PDOException $e)
+     {
+         echo $e->getMessage();
+     }
+     $conn = null;
+ 
+
 
 ?>
 </html>
